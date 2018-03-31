b4ba0f69ca30204c1c2f9937c9a9b6cb16ff3f80
victoriaasia <yes.im@bk.ru>
2018-03-31 19:34:24 +0300

    vol2

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+	app.init();
+});
+
 var app = app || {
 	init: function(){
 		app.slide_functions();
@@ -14,12 +18,11 @@ var app = app || {
 				.css("-moz-transform","translateY("+new_position+")")
 				.css("-ms-transform","translateY("+new_position+")")
 				.css("-o-transform","translateY("+new_position+")");
+			} else {
+				//Legacy support
+				$(".js-slider").css("top",-(num-1)*100+"%");
 
-		} else {
-			//Legacy support
-			$(".js-slider").css("top",-(num-1)*100+"%");
-
-		}
+			}
 		app.current_slide = num;
 	},
 
@@ -37,42 +40,38 @@ var app = app || {
 		});
 	}
 }
-$(document).ready(function(){
-	app.init();
-});
 
-
-const boxer = mainSlideContainer.querySelector(".js-people"),
-			woman = mainSlideContainer.querySelector(".js-woman"),
-			maxMove = mainSlideContainer.offsetWidth / 30,
-			boxerCenterX = boxer.offsetLeft + (boxer.offsetWidth / 2),
+const people = introContainer.querySelector(".js-people"),
+			woman = introContainer.querySelector(".js-woman"),
+			maxMove = introContainer.offsetWidth / 30,
+			peopleCenterX = people.offsetLeft + (people.offsetWidth / 2),
 			womanCenterX = woman.offsetLeft + (woman.offsetWidth / 2),
-			boxerCenterY = boxer.offsetTop + (boxer.offsetHeight / 2),
+			peopleCenterY = people.offsetTop + (people.offsetHeight / 2),
 			womanCenterY = woman.offsetTop + (woman.offsetHeight / 2),
-			fluidboxer = window.matchMedia("(min-width: 726px)");
+			fluidpeople = window.matchMedia("(min-width: 726px)");
 
 function getMousePos(xRef, yRef) {
 
-  let panelRect = mainSlideContainer.getBoundingClientRect();
+  let panelRect = introContainer.getBoundingClientRect();
   return {
-	  x: Math.floor(xRef - panelRect.left) /(panelRect.right-panelRect.left)*mainSlideContainer.offsetWidth,
-	  y: Math.floor(yRef - panelRect.top) / (panelRect.bottom -panelRect.top) * mainSlideContainer.offsetHeight
+	  x: Math.floor(xRef - panelRect.left) /(panelRect.right-panelRect.left)*introContainer.offsetWidth,
+	  y: Math.floor(yRef - panelRect.top) / (panelRect.bottom -panelRect.top) * introContainer.offsetHeight
     };
 }
 
 document.body.addEventListener("mousemove", function(e) {
 
   let mousePos = getMousePos(e.clientX, e.clientY),
-  distX = mousePos.x - boxerCenterX,
-  distY = mousePos.y - boxerCenterY,
+  distX = mousePos.x - peopleCenterX,
+  distY = mousePos.y - peopleCenterY,
 
       womandistX = mousePos.x - womanCenterX,
       womandistY = mousePos.y - womanCenterY;
 
-  if (Math.abs(distX) < 800 && distY < 300 && fluidboxer.matches) {
-  boxer.style.transform = "translate("+(-1*distX)/12+"px,"+(-1*distY)/12+"px)";
-      woman.style.transform = "translate("+(-1*distX)/-12+"px,"+(-1*distY)/-12+"px)";
-    mainSlideContainer.style.backgroundPosition = `calc(87% + ${distX/50}px) calc(87% + ${distY/50}px)`;
+  if (Math.abs(distX) < 800 && distY < 300 && fluidpeople.matches) {
+		people.style.transform = "translate("+(-1*distX)/12+"px,"+(-1*distY)/12+"px)";
+		woman.style.transform = "translate("+(-1*distX)/-12+"px,"+(-1*distY)/-12+"px)";
+		introContainer.style.backgroundPosition = `calc(87% + ${distX/50}px) calc(87% + ${distY/50}px)`;
   }
 
-})
+});
